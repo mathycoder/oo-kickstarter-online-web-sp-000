b2e7b06965e990f2c29491df3a2406d4c002d80e
Adam S <adam.sarli@gmail.com>
2019-03-19 19:40:02 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer 
+  attr_reader :backed_projects, :name
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end 
+  
+  def back_project(project)
+    @backed_projects << project 
+    if !(project.backers.include?(project))
+      project.add_backer(self)
+    end 
+  end 
+  
+end 
