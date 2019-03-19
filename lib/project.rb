ba276a195dbc881716b53edc27926399d3c5c6eb
Adam S <adam.sarli@gmail.com>
2019-03-19 19:39:17 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,8 +8,9 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    if backer.
-    backer.back_project(self)
+    if !(backer.backed_projects.include?(self))
+      backer.back_project(self)
+    end 
   end 
   
 end 
